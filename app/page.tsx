5ddc725d79a88e1c474bddd24698721bca09c237
Keanu Reaño <reanokeanu@gmail.com>
2024-10-25 10:34:49 +0800

    Added new styles to the global CSS file, defining color variables for light and dark themes. Also added base styles for different color themes in the CSS file. Updated the package.json file with new dependencies and devDependencies. Created a new components.json file defining component aliases. Added a new utility function in lib/utils.ts file. Deleted the image1.png file. Updated the tailwind configuration file to define dark mode settings and extend color styles.

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,3 @@
-import Image from "next/image";
-
 export default function Home() {
-  return (
-    <div>
-      <Image src="./image1.png" alt="Image" width={1280} height={1920} />
-    </div>
-  );
+  return <div>Hello World</div>;
 }
